fbaabd3f97b962ea91b59bd9e3a54f8ddad4c9e1
Cade Ekblad-Frank <cade@e-f.me>
2020-11-16 23:47:13 -0600

    Mirror changes in cookiecutter-base

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -15,3 +15,24 @@ git add .pre-commit-config.yaml
 
 # Install imports
 poetry install
+
+# Create GitHub repository?
+CREATE_GH_REPO="{{ cookiecutter.create_gh_repo }}"
+
+if [[ ${CREATE_GH_REPO} == "yes" ]]; then
+	CREATE_GH_REPO_VISIBILITY="{% if cookiecutter.create_gh_repo_visibility == 'public' %}--public{% else %}--private{% endif %}"
+	gh repo create --confirm "${CREATE_GH_REPO_VISIBILITY}" --description "{{ cookiecutter.repo_description }}" "{{ cookiecutter.repo_name }}"
+fi
+
+# Clean up unwanted directories
+# Necessary because cookiecutter's handling of empty filenames (perfectly fine)
+# and empty directories (definitely not) is inconsistent.
+# I should properly debug one of these days
+
+# Remove .github if workflows aren't in use
+ENABLE_GH_ACTIONS="{{ cookiecutter.enable_gh_actions }}"
+
+if [[ "${ENABLE_GH_ACTIONS}" == "no" ]]; then
+	echo "Removing .github..."
+	rm -vrf ./.github
+fi
--- {{ cookiecutter.repo_name }}/README.md
+++ {{ cookiecutter.repo_name }}/README.md
@@ -1,6 +1,5 @@
 # {{ cookiecutter.repo_name }}
-
-{% if cookiecutter.project_type == "cli" %}
+{% if cookiecutter.project_type == "cli" and cookiecutter.script_name %}
 ## {{ cookiecutter.script_name }}
 
 ### Installation
